246f0aac4a5440ab851868f2076a84e90abf6e27
Anna Zinchenko <anna.s.zinchenko@gmail.com>
2022-11-05 17:07:58 +0200

    Tests added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,48 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void remain1000Test() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remain999Test() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(1001);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remain1Test() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(999);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remain500Test() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(2500);
+        int expected = 500;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remainAmount1Test() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(1);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+}
